a7be82efdf6a0184fc925b076464ef5f72a44b1f
EC2 Default User <ec2-user@ip-172-31-9-123.ec2.internal>
2022-02-19 10:15:23 +0000

    [Add]View_quit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,7 +1,31 @@
 class Public::CustomersController < ApplicationController
+
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+    if @user == current_customer
+      render :edit
+    else
+      redirect_to customers_path(current_customer.id)
+    end
   end
+
+  def update
+    @customer = Costomer.find(params[:id])
+    if @user.update(user_params)
+      redirect_to customers_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+
+  def quit
+  end
+
+  def out
+  end
+
 end
