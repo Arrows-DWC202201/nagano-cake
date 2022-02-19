72c41ba64b64a93c60d9a40fcda2d25c28f3e465
EC2 Default User <ec2-user@ip-172-31-1-111.ec2.internal>
2022-02-19 07:39:43 +0000

    [Fix]deviseのlogoutとroutes

--- config/routes.rb
+++ config/routes.rb
@@ -42,5 +42,6 @@ Rails.application.routes.draw do
   devise_scope :customer do
     get '/customers/sign_out' => 'devise/sessions#destroy'
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
