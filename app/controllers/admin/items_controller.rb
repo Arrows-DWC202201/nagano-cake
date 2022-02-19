70967e66b535a2e6402a59ab6a62045fcc8035e4
NoriakiNakamura <babababargxx@gmail.com>
2022-02-19 11:48:26 +0000

    [ADD_SAVE]admin/item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,45 @@
 class Admin::ItemsController < ApplicationController
-  def index
-  end
 
-  def new
+  def index
+    # @items = Item.all
+    @items = Item.page(params[:page]).per(10)
   end
 
   def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render :new
+    end
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :imaage_id, :is_active)
+  end
+
 end
