41deae35515b17f55d900a06c3bbdd7b9f700844
EC2 Default User <ec2-user@ip-172-31-1-111.ec2.internal>
2022-02-21 03:18:41 +0000

    [Add]items/cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,41 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+  end
+
+  def create
+    @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
+    if @cart_item.present?
+      @cart_item.quantity += params[:quantity].to_i
+    else
+      @cart_item = CartItem.new(cart_item_params)
+    end
+
+
+    @cart_item.save
+    redirect_to items_path
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_item_path
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
+  end
+
+  def all_destroy
+    current_customer.cart_items.destroy_all
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :quantity)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :cart_items, dependent: :destroy
+
+  def subtotal
+    item.with_tax_price * quantity
+  end
+
 end
