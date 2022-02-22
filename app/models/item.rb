8dbee89240a2e0a366bcefb97dae1b0dd5ba331b
NoriakiNakamura <babababargxx@gmail.com>
2022-02-22 05:07:21 +0000

    [ADD]order_items:model,controller & admin/orders/show ::before_test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
 
   belongs_to :genre
+  has_many :order_items, dependent: :destroy
 
   attachment :image
 
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,31 @@
 class OrderItem < ApplicationRecord
+
+  after_update :check_make_status
+
+  belongs_to :order
+  belongs_to :item
+
+  enum make_status: {
+    impossible: 0,
+    wating:     1,
+    making:     2,
+    finish:     3
+  }
+
+  def sum_of_price
+    (self.tax_price * self.quantity).round
+  end
+
+  private
+
+  def check_make_status
+    if self.make_status == "making"
+      self.order.update(status: "making")
+    elsif self.make_status == "finish"
+      if self.order.order_items.all? { |order_item| order_item.make_status == "finish" }
+        self.order.update(status: "ready_to_ship")
+      end
+    end
+  end
+
 end
