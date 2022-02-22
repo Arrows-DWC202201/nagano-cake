b41d566df95ce76662e4e24cad4048ebf2ee335a
NoriakiNakamura <babababargxx@gmail.com>
2022-02-22 09:28:34 +0000

    [for_save]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,10 +1,31 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = current_customer.orders.new
+    @address = Address.new
   end
 
   def index
+    @orders = current_customer.orders.order(created_at: :desc)
   end
 
   def show
   end
+
+  def thanks
+  end
+
+  def confirm
+
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :payment_method, :postal_code, :address, :total_payment)
+  end
+
+  def address_params
+    params.permit(:name,:address,:postal_code)
+  end
+
 end
