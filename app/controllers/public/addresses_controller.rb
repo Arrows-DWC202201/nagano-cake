df28c0c470cd05468e0183db53ad923fc1175380
EC2 Default User <ec2-user@ip-172-31-9-123.ec2.internal>
2022-02-22 13:34:10 +0000

    [Add]addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,60 @@
 class Public::AddressesController < ApplicationController
+
   def index
+    @address = Address.new
+    @customer = current_customer
+    @addresses = Address.where(customer_id: @customer.id)
+  end
+
+  def new
+    @address = Address.new
+  end
+
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      @customer = current_customer
+      @addresses = Address.where(customer_id: @customer.id)
+      render :index
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
+    if @address.customer == current_customer
+      render :edit
+    else
+      redirect_to address_path
+    end
   end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    address = Address.find(params[:id])
+    if address.destroy
+      redirect_to addresses_path
+    else
+      render :index
+    end
+  end
+
+
+
+  private
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,7 @@
 class Address < ApplicationRecord
+  belongs_to :customer
+
+  validates :customer_id, :postal_code, :address, :name, presence: true
+  validates :postal_code, numericality: {only_integer: true}, length: { is: 7 }
+
 end
