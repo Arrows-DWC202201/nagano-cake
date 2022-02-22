bbf2c811f36c4cda186afd9672ee5e024006288d
NoriakiNakamura <babababargxx@gmail.com>
2022-02-22 15:05:01 +0000

    [ for_merge ]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,13 +9,42 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+    @order = current_customer.orders.find(params[:id])
+    @order.carriage = 800
   end
 
   def thanks
   end
 
   def confirm
+    @order = current_customer.orders.new
+    @order.payment_method = params[:payment_method]
+    @order.carriage = 800
+    @order.total_payment = current_customer.cart_items_total_price + @order.carriage
 
+    receiver =
+    case @address_type = params[:address_type].to_i
+    when 1
+      current_customer
+    when 2
+      current_customer.addresses.find(params[:address_id])
+    when 3
+      @address = current_customer.addresses.new(address_params)
+      unless @address.save
+        flash[:alert] = '住所が正しくありません'
+        render :new
+        return
+      else
+        @address
+      end
+    else
+    end
+    @order.set_receiver(receiver)
+  end
+
+  def create
+    current_customer.orders.create(order_params)
+    redirect_to complete_orders_path
   end
 
   private
@@ -26,6 +55,10 @@ class Public::OrdersController < ApplicationController
 
   def address_params
     params.permit(:name,:address,:postal_code)
+  end
+
+  def check_cart_is_not_empty
+    redirect_to items_path unless current_customer.cart_items.present?
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -13,6 +13,48 @@ class Order < ApplicationRecord
     sent: 4
   }
 
+  def set_receiver(receiver)
+    self.address = receiver.address
+    self.postal_code = receiver.postal_code
+    if receiver.is_a?(Customer)
+      self.name = receiver.full_name.gsub(" ", "")
+    else
+      self.name = receiver.name
+    end
+  end
+
+  def order_items_total_price
+    (total_payment - 800).round
+  end
+
+  def order_items_total_quantity
+    self.order_items.sum(:quantity)
+  end
+
+
+  after_create :move_cart_items
+  after_update :check_order_item
+
+  private
+
+  def move_cart_items
+    cart_items_list = self.customer.cart_items.map do |cart_item|
+      {
+        item_id: cart_item.item_id,
+        tax_price: cart_item.item.with_tax_price,
+        quantity: cart_item.quantity
+      }
+    end
+    self.order_items.create(cart_items_list)
+    self.customer.cart_items.destroy_all
+  end
+
+  def check_order_item
+    if self.order_status == "confirm_payment"
+      self.order_items.update_all(make_status: "wating")
+    end
+  end
+
 
 
 end
