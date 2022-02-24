0d1454420703c0a56f5582df1a4837d548b11401
NoriakiNakamura <babababargxx@gmail.com>
2022-02-24 23:32:20 +0000

    [Improve]design_naka

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,9 +2,7 @@ class Admin::OrderItemsController < ApplicationController
 
   def update
     @order_item = OrderItem.find(params[:id])
-    if @order_item.update(order_item_params)
-       flash[:notice] = "製作ステータスの更新に成功しました"
-    end
+    @order_item.update(order_item_params)
     redirect_to admin_order_path(@order_item.order)
   end
 
